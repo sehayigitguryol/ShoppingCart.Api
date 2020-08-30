b2f26cf72fab3d05b0f8fe6c1b4f1f1e08fff9e3
Seha Yiğit Güryol <s.yigitguryol@gmail.com>
2020-08-30 20:10:01 +0300

    swagger

--- ShoppingCart.Api/Startup.cs
+++ ShoppingCart.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace ShoppingCart.Api
 {
@@ -26,6 +28,16 @@ namespace ShoppingCart.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Shopping Cart API",
+                    Version = "v1",
+                    Description = "Shopping Cart API descripiton page",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +53,14 @@ namespace ShoppingCart.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.RoutePrefix = "help";
+                s.SwaggerEndpoint("../swagger/v1/swagger.json", "Shopping Cart");
+                s.InjectStylesheet("../css/swagger.min.css");
+            });
 
             app.UseEndpoints(endpoints =>
             {
