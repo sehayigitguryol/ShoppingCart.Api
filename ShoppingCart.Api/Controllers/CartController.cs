4e0671f93b2a73fc93c109ea09e72d60d35ecfcf
Seha Yiğit Güryol <s.yigitguryol@gmail.com>
2020-09-01 20:40:35 +0300

    cart controller

---
+++ ShoppingCart.Api/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CartController: ControllerBase
+    {
+        private readonly ICartService cartService;
+
+        public CartController(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
